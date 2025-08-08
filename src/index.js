41d2c777ddfa614df678041c3751306f48ba994e
LuanGale <lgaletascon@gmail.com>
2025-08-08 12:27:35 +0200

    update date and time with current

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+function updateTime(Time) {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
 
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = `${londonTime.format(
+    "HH:mm:ss"
+  )} <small>${londonTime.format("A")}`;
+
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+
+  let madridTime = moment().tz("Europe/Madrid");
+
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = `${madridTime.format(
+    "HH:mm:ss"
+  )} <small>${madridTime.format("A")}`;
+}
+
+setInterval(updateTime, 1000);
